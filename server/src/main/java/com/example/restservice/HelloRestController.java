72de811d95bc4888bd38338faa8c8d252b289ac9
swdev.smz@gmail.com <swdev.smz@gmail.com>
2019-04-29 22:58:13 +0900

    テストコードの追加

--- server/src/main/java/com/example/restservice/HelloRestController.java
+++ server/src/main/java/com/example/restservice/HelloRestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloRestController {
 
     @GetMapping
+    @RequestMapping("show")
     public String showHelloWorld(){
         return "hello world";
     }
