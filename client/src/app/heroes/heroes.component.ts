15b93d5a66093a62e96d154d12e7c1bab5c5f771
swdev.smz@gmail.com <swdev.smz@gmail.com>
2019-07-14 22:09:52 +0900

    hero list　を追加

--- client/src/app/heroes/heroes.component.ts
+++ client/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from　'../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -13,9 +14,15 @@ export class HeroesComponent implements OnInit {
     name: 'WindStorm'
   }
 
+  heroes = HEROES;
+  selectedHero: Hero;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
