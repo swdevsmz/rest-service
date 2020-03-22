e0a66c76d20c54ae752c6b4b1c8f4b93cb8f4ddf
swdev.smz@gmail.com <swdev.smz@gmail.com>
2020-03-22 11:24:17 +0900

    angula rversion up

---
+++ server/src/main/java/com/example/restservice/presentation/DMMRestController.java
@@ -0,0 +1,13 @@
+package com.example.restservice.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/dmm")
+@RequiredArgsConstructor
+public class DMMRestController {
+
+
+}
