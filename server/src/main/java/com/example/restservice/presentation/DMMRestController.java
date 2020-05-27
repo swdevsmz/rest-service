0966804707b93238046b0e0e6ec9a0957206a848
swdev.smz@gmail.com <swdev.smz@gmail.com>
2020-05-27 23:26:33 +0900

    logbackの追加

--- server/src/main/java/com/example/restservice/presentation/DMMRestController.java
+++ server/src/main/java/com/example/restservice/presentation/DMMRestController.java
@@ -1,13 +1,22 @@
 package com.example.restservice.presentation;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("api/dmm")
 @RequiredArgsConstructor
 public class DMMRestController {
 
+  private final RestTemplate restTemplate;
+
+  @GetMapping(value = "show")
+  public String show() {
+    String url = "";
+    return restTemplate.getForObject(url, String.class);
+  }
 
 }
